cee53fc8082291dfef59b20c2145aee5dc3cd7b5
Eiwe Lingefors <email@eiwe.me>
2012-02-22 21:59:57 -0500

    Adding a second solution and fixing the other a bit

--- day7/simple_rack2.rb
+++ day7/simple_rack2.rb
@@ -5,4 +5,5 @@ def rack_method env
   [200, {"Content-Type"=>"text/plain"}, ["Command line argument you typed was: #{ARGV.join(' ')}"]]
 end
 
-Rack::Server.new( { :app => method(:rack_method), :server => 'webrick', :Port => 8500} ).start
+
+Rack::Handler::Thin.run method(:rack_method), :Port => 8500
